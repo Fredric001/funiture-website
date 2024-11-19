e449d33e93fd1d28a5336ed13c207622e5a7de97
fredric othiambo <fredricothiambo19@gmail.com>
2024-11-19 15:11:32 +0300

    commit

---
+++ assets/sub-pages/cart.js
@@ -0,0 +1,9 @@
+
+
+let mainTwoCont= document.getElementById("main-two-cont")
+let goToCart= document.getElementById("go-Cart")
+
+goToCart.addEventListener("click",()=>{
+    mainTwoCont.style.display="grid"
+    mainTwoCont.style.position="absolute" 
+})
